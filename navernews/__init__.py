0624bc09bb12bd21f59accb3ac063c1ca8eeaaf2
bae jiun, maybe <alice.maydev@gmail.com>
2018-05-12 18:54:15 +0900

    Fix utility, Update init module packages

--- navernews/__init__.py
+++ navernews/__init__.py
@@ -3,4 +3,8 @@ Naver News
 """
 __version__ = '1.0.0'
 
-from pythond.pythond import Daemon
+__all__ = [
+    'get_press', 'get_category'
+]
+
+from navernews.lib.utility import get_press, get_category
--- navernews/lib/utility.py
+++ navernews/lib/utility.py
@@ -1,18 +1,33 @@
+from typing import Generator, Tuple, List
+
 from lxml import html
 
-from .connectons import request
+from .connections import request
 
-def html(context)
-    return html.fromstring(context)
 
-def get_press():
+def get_context(text) -> html.HtmlElement:
+    """ get html document context
+    """
+    return html.fromstring(text)
+
+
+def get_press(label: bool = True) -> Generator[str]:
+    """ get press list
+    """
+    ext = lambda e: e.text if label else e.get('class').split(',')[1][:-1]
     url = 'http://news.naver.com/main/ajax/bottomIndex/press.nhn'
-    context = html(request(url))
-    for press in context.xpath('//li/a'):
-        yield press.text
+    for press in get_context(request(url)).xpath('//ul/li/a'):
+        if ',' in press.get('class'):
+            yield ext(press)
 
-def get_category():
+
+def get_category(label: bool = True) -> Generator[Tuple[str, List[str]]]:
+    """ get category list
+    """
+    ext = lambda e: e.text if label else e.get('class').split(',')[1][:-1]
     url = 'http://news.naver.com/main/ajax/bottomIndex/category.nhn'
-    context = html(request(url))
-    for cate in context.xpaht('//h5/a'):
-        yield cate.text
+    for div in get_context(request(url)).xpath('//div'):
+        if not div.xpath('./h5/a'): continue
+        yield ext(div.xpath('./h5/a')[0]), [
+            ext(sub) for sub in div.xpath('./ul/li/a') if ',' in sub.get('class')
+        ]
