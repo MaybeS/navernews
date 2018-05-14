0e1b36df2b08899fc0ad8ca6ae6625594bc44c22
Maydev <alice.maydev@gmail.com>
2018-05-14 19:16:56 +0900

    Update handler

--- navertools/lib/handler.py
+++ navertools/lib/handler.py
@@ -1,1 +1,31 @@
 import multiprocessing as mp
+from typing import Callable, List
+
+class BaseHandler:
+    """ Handling tasks efficiently
+    """
+    def __init__(self, n: int = mp.cpu_count()):
+        self.pool = mp.Pool(n)
+
+    def task(args):
+        """ Override
+        """
+        pass
+
+    def callback(result):
+        """ Override
+        """
+        pass
+
+    def run(args: List):
+        self.result = self.pool.map_async(self.task, args, callback=self.callback)
+        self.result.wait()
+
+
+class Handler(BaseHandler):
+    """ Handling tasks efficiently
+    """
+    def __init__(self, task: Callable, callback: Callable = lambda g: None, n: int = mp.cpu_count()):
+        super().__init__(n)
+        self.task = task
+        self.callback = callback
