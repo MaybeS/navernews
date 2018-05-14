63724b45181d007857a6728bde4573adb5afcbc7
Maydev <alice.maydev@gmail.com>
2018-05-14 10:50:09 +0900

    Update tests utility

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -8,26 +8,32 @@
 
 import pytest
 
-from navernews import get_press, get_category
+from navertools import News
+
+@pytest.fixture
+def instance():
+    return News()
 
 def test_press():
     """ Testing for get_press
     """
-    for press in get_press():
-        assert isinstance(press) == str
+    news = instance()
+    for press in news.get_press():
+        assert isinstance(press, str)
 
-    for press in get_press(label=False):
-        assert isinstance(press) == str
+    for press in news.get_press(label=False):
+        assert isinstance(press, str)
 
 def test_category():
     """ Testing for get_category
     """
-    for major, minors in get_category():
-        assert isinstance(major) == str
+    news = instance()
+    for major, minors in news.get_category():
+        assert isinstance(major, str)
         for minor in minors:
-            assert isinstance(minor) == str
+            assert isinstance(minor, str)
 
-    for major, minors in get_category(label=False):
-        assert isinstance(major) == str
+    for major, minors in news.get_category(label=False):
+        assert isinstance(major, str)
         for minor in minors:
-            assert isinstance(minor) == str
+            assert isinstance(minor, str)
